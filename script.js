c30d8cefab18f68445a76df532aee125eee4cff5
alow-akter <kironalow453@gmail.com>
2022-09-15 15:36:03 -0400

    error count commit

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const typeController = (e) => {
 
   // Handle backspace press
   if (newLetter == "Backspace") {
-
+    errorCount++;
     userText = userText.slice(0, userText.length - 1);
     return display.removeChild(display.lastChild);
   }
